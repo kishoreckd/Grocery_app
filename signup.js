0c4fa6979d834959272f93c1916c5882fe386b60
swathiramesh20 <swathisrir@dckap.com>
2023-04-24 15:59:48 +0530

    Products_Api_Updated

--- signup.js
+++ signup.js
@@ -1,7 +1,5 @@
 let Firstname = document.querySelector(".Firstname")
-
 let LastName = document.querySelector(".LastName")
-
 let userEmail = document.querySelector(".email")
 
 let Password = document.querySelector(".Password")
@@ -14,44 +12,41 @@ let Passwordalat = document.querySelector(".Passwordalat")
 
 
 
-submitbtn.addEventListener("click",(e)=>{
+submitbtn.addEventListener("click", (e) => {
     e.preventDefault()
 
     // alert("hello")
 
-    if(Firstname.value && LastName.value && userEmail.value && Password.value && Confirmpassword.value) {
-        if(Password.value == Confirmpassword.value) {
-        fetch("http://localhost:3000/users", {
-            method: 'POST',
-            body: JSON.stringify({
-                Firstname: `${Firstname.value}`,
-                LastName: `${LastName.value}`,
-                Email: `${userEmail.value}`,
-                Password: `${Password.value}`,
-                Confirmpassword: `${Confirmpassword.value}`
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            // console.log(data);
+    if (Firstname.value && LastName.value && userEmail.value && Password.value && Confirmpassword.value) {
+        if (Password.value == Confirmpassword.value) {
+            fetch("http://localhost:3000/users", {
+                method: 'POST',
+                body: JSON.stringify({
+                    Firstname: `${Firstname.value}`,
+                    LastName: `${LastName.value}`,
+                    Email: `${userEmail.value}`,
+                    Password: `${Password.value}`,
+                    Confirmpassword: `${Confirmpassword.value}`
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                }
+            })
+                .then((response) => response.json())
+                .then((data) => {
 
-
-
-        })
-        .catch(err => console.log(err))
-    }
-    else{
-
-        Passwordalat.innerText = "please enter the Correct password"
-       
-        setTimeout(() => {
-            Passwordalat.innerText = ""
-        }, 1000);
-    }
+                })
+                .catch(err => console.log(err))
         }
+        else {
+
+            Passwordalat.innerText = "please enter the Correct password"
+
+            setTimeout(() => {
+                Passwordalat.innerText = ""
+            }, 1000);
+        }
+    }
 
 
 })
