4c1d7d58c53071d85fa61c0e9ad51e8c25a6181c
swathiramesh20 <swathisrir@dckap.com>
2023-04-24 19:04:31 +0530

    signup_functionality

--- signup.js
+++ signup.js
@@ -8,23 +8,53 @@ let Password = document.querySelector(".Password")
 
 let Confirmpassword = document.querySelector(".Confirmpassword")
 
-let submitbtn = document.querySelector(".signbtn")
+let signup = document.querySelector(".signbtn")
 
-let Passwordalat = document.querySelector(".Passwordalat")
+let Password_alert = document.querySelector(".Password_alert")
 
 let passwordicon = document.querySelector("#passwordicon")
 
 let passwordiconsee = document.querySelector(".passwordiconsee")
 
+let user_alert = document.querySelector(".user_alert")
+
 
 passwordicon.addEventListener("click", (e) => userseepassword(e))
 
+/* ---------------------------------------------------------------------------------------- */
+/* When the signup button is clicked the input value is assigned to 
+the local database by the POST Method*/
 
-submitbtn.addEventListener("click", (e) => gettingdata_from_user(e))
+signup.addEventListener("click", (e) => {
+    e.preventDefault()
+    /* Before storing t the local database this fetch checks whether the user is already 
+    in database*/
+    fetch(`http://localhost:3000/users/?Email=${userEmail.value}`)
+        .then(data => data.json())
+        .then(data => {
 
+            /* If the user Database is stored it returns the value of 0 */
+            if (data.length == 0) {
+                /* This function is used to create the database for users whether the user 
+                is new*/
 
+                gettingdata_from_user()
+            }
+            else {
+                /* If the user is already existed it alerts the user*/
+
+                user_alert.classList.add("visible")
+
+                setTimeout(() => {
+                    user_alert.classList.remove("visible")
+                }, 1000);
+            }
+        })
+})
+/* ---------------------------------------------------------------------------------------- */
+/* This function checks whether the user inputs the password is equal to confirm password*/
 function userseepassword(e) {
-        
+
     if (Password.type == " password") {
         Password.type = "password"
 
@@ -33,17 +63,19 @@ function userseepassword(e) {
         Password.type = "text"
         passwordiconsee.style.display = "block"
         passwordicon.style.display = "none"
-        
+
     }
 }
 
 
-
-function gettingdata_from_user(e) {
+/* ---------------------------------------------------------------------------------------- */
+/*This function used to create the data for the user*/
+function gettingdata_from_user() {
     {
-        e.preventDefault()
+
 
         if (Firstname.value && LastName.value && userEmail.value && Password.value && Confirmpassword.value) {
+
             if (Password.value == Confirmpassword.value) {
                 fetch("http://localhost:3000/users", {
                     method: 'POST',
@@ -66,10 +98,10 @@ function gettingdata_from_user(e) {
             }
             else {
 
-                Passwordalat.innerText = "please enter the Correct password"
+                Password_alert.innerText = "please enter the Correct password"
 
                 setTimeout(() => {
-                    Passwordalat.innerText = ""
+                    Password_alert.innerText = ""
                 }, 1000);
             }
         }
@@ -78,3 +110,5 @@ function gettingdata_from_user(e) {
 
     }
 }
+
+/* ---------------------------------------------------------------------------------------- */
