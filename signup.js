91d97593ab8f3e6feb9af88911c95101d76490b3
swathiramesh20 <swathisrir@dckap.com>
2023-04-24 17:58:02 +0530

    API Updated

--- signup.js
+++ signup.js
@@ -1,5 +1,7 @@
 let Firstname = document.querySelector(".Firstname")
+
 let LastName = document.querySelector(".LastName")
+
 let userEmail = document.querySelector(".email")
 
 let Password = document.querySelector(".Password")
@@ -10,45 +12,66 @@ let submitbtn = document.querySelector(".signbtn")
 
 let Passwordalat = document.querySelector(".Passwordalat")
 
+let passwordicon = document.querySelector("#passwordicon")
+
+let passwordiconsee = document.querySelector(".passwordiconsee")
 
 
-submitbtn.addEventListener("click",(e)=>{
-    e.preventDefault()
+passwordicon.addEventListener("click", (e) => userseepassword(e))
 
-    // alert("hello")
 
-    if(Firstname.value && LastName.value && userEmail.value && Password.value && Confirmpassword.value) {
-        if(Password.value == Confirmpassword.value) {
-        fetch("http://localhost:3000/users", {
-            method: 'POST',
-            body: JSON.stringify({
-                Firstname: `${Firstname.value}`,
-                LastName: `${LastName.value}`,
-                Email: `${userEmail.value}`,
-                Password: `${Password.value}`,
-                Confirmpassword: `${Confirmpassword.value}`
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
+submitbtn.addEventListener("click", (e) => gettingdata_from_user(e))
+
+
+function userseepassword(e) {
+        
+    if (Password.type == " password") {
+        Password.type = "password"
+
+    }
+    else {
+        Password.type = "text"
+        passwordiconsee.style.display = "block"
+        passwordicon.style.display = "none"
+        
+    }
+}
+
+
+
+function gettingdata_from_user(e) {
+    {
+        e.preventDefault()
+
+        if (Firstname.value && LastName.value && userEmail.value && Password.value && Confirmpassword.value) {
+            if (Password.value == Confirmpassword.value) {
+                fetch("http://localhost:3000/users", {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        Firstname: `${Firstname.value}`,
+                        LastName: `${LastName.value}`,
+                        Email: `${userEmail.value}`,
+                        Password: `${Password.value}`,
+                        Confirmpassword: `${Confirmpassword.value}`
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    }
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+
+                    })
+                    .catch(err => console.log(err))
             }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            // console.log(data);
+            else {
 
+                Passwordalat.innerText = "please enter the Correct password"
 
-
-        })
-        .catch(err => console.log(err))
-    }
-    else{
-
-        Passwordalat.innerText = "please enter the Correct password"
-       
-        setTimeout(() => {
-            Passwordalat.innerText = ""
-        }, 1000);
-    }
+                setTimeout(() => {
+                    Passwordalat.innerText = ""
+                }, 1000);
+            }
         }
 
 
